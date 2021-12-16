0b39fde55f006e0a7bb6faab0bb24e6ae1712632
class <classicalbloshi@gmail.com>
2021-12-16 17:34:46 +0400

    edit metadata is implemented

--- app/src/main/java/uk/ac/shef/oak/com4510/ShowImageActivity.kt
+++ app/src/main/java/uk/ac/shef/oak/com4510/ShowImageActivity.kt
@@ -16,6 +16,20 @@ import uk.ac.shef.oak.com4510.data.ImageDataDao
 class ShowImageActivity : AppCompatActivity() {
     lateinit var daoObj: ImageDataDao
 
+    val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            val position = result.data?.getIntExtra("position", -1)
+            val id = result.data?.getIntExtra("id", -1)
+            val del_flag = result.data?.getIntExtra("deletion_flag", -1)
+            var intent = Intent().putExtra("position", position)
+                .putExtra("id", id).putExtra("deletion_flag", del_flag)
+            when (result.resultCode) {
+                Activity.RESULT_OK -> {
+                    this.setResult(result.resultCode, intent)
+                    this.finish()
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,21 +50,19 @@ class ShowImageActivity : AppCompatActivity() {
     private fun displayData(position: Int){
         if (position != -1) {
             val imageView = findViewById<ImageView>(R.id.show_image)
-            val titleToolbar = findViewById<Toolbar>(R.id.show_toolbar)
             val descriptionTextView = findViewById<TextView>(R.id.show_image_description)
-            val imageData = MyAdapter.items[position]
 
             imageView.setImageBitmap(MyAdapter.items[position].thumbnail!!)
             descriptionTextView.text = MyAdapter.items[position].imageDescription
 
             val fabEdit: FloatingActionButton = findViewById(R.id.fab_edit)
-            /*fabEdit.setOnClickListener(View.OnClickListener {
+            fabEdit.setOnClickListener(View.OnClickListener {
                 startForResult.launch(
                     Intent( this, EditActivity::class.java).apply {
                         putExtra("position", position)
                     }
                 )
-            })*/
+            })
         }
     }
 }
